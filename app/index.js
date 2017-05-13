cd4a7a244fd45e0658fcdef9d6f7f43abf3afd5b
Honohonoho <751718620@qq.com>
2017-05-13 16:16:44 +0800

    fix bugs

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash'
+import j from 'jquery'
+import liqi from './liqi'
+function component () {
+  var element = j('<div></div>');
+  /* lodash is required for the next line to work */
+  element.html(_.join(['Hello','webpack'],' '))
+
+  return element.get(0);
+}
+
+document.body.appendChild(component());
+//console.log(liqi)
+console.log(liqi())
